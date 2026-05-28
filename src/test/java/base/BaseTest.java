0be96ac0bad1c3febd17bd6b7da44c4180f54d6c
AleksandraPrlincevic <aleksandra.prlincevic@gmail.com>
2026-05-28 21:37:03 +0200

    Set BaseTest and LoginTest basics

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,23 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,37 @@
 package tests;
 
-public class LoginTest {
+import base.BaseTest;
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+public class LoginTest  extends BaseTest {
+
+
+
+
+    @BeforeMethod
+    public void testSetUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.saucedemo.com/");
+    }
+
+
+
+    @AfterMethod
+    public void deleteCookies(){
+        driver.manage().deleteAllCookies();
+    }
+
 }
