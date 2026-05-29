ed556a4631e22190453a1d7a673530eaf2dea78d
AleksandraPrlincevic <aleksandra.prlincevic@gmail.com>
2026-05-29 19:03:04 +0200

    Adding locators for InventoryPage

--- src/test/java/pages/InventoryPage.java
+++ src/test/java/pages/InventoryPage.java
@@ -1,4 +1,27 @@
 package pages;
 
-public class InventoryPage {
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class InventoryPage extends BasePage {
+
+    public InventoryPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //-----------------------Lokatori--------------------
+
+    @FindBy(className = "shopping_cart_link")
+    public WebElement cartIcon;
+
+    @FindBy(id="add-to-cart-sauce-labs-backpack")
+    public  WebElement addToCartButtonBackpack;  //sta kad taj backpack prestane da se prodaje?
+
+    @FindBy(css="select[className='product_sort_container']")
+    public WebElement productSortButton;
+
+
+
 }
